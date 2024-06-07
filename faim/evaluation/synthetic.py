7dd47d05a891ce2fea5522c140ad9d9d1c440f6c
Alex Loosley <aloosley@alumni.brown.edu>
2024-06-07 20:07:25 +0200

    MMC coefs in eval

--- faim/evaluation/synthetic.py
+++ faim/evaluation/synthetic.py
@@ -65,13 +65,28 @@ def evaluate(data: pd.DataFrame, pathToResult: str) -> None:
     resultString += perfDiffs.to_string()
 
     # calc diffs in error rates
-    errDiffs = pd.DataFrame(columns=["raw FPR", "raw FNR", "fair FPR", "fair FNR", "raw - fair FPR", "raw - fair FNR"])
+    errDiffs = pd.DataFrame(
+        columns=[
+            "raw FPR",
+            "raw FNR",
+            "raw MCC",
+            "fair FPR",
+            "fair FNR",
+            "fair MCC",
+            "raw - fair FPR",
+            "raw - fair FNR",
+            "raw - fair MCC",
+        ]
+    )
     errDiffs["raw FPR"] = rawScoreErrorRates["FPR"]
     errDiffs["raw FNR"] = rawScoreErrorRates["FNR"]
+    errDiffs["raw MCC"] = rawScoreErrorRates["MCC"]
     errDiffs["fair FPR"] = fairScoreErrorRates["FPR"]
     errDiffs["fair FNR"] = fairScoreErrorRates["FNR"]
+    errDiffs["fair MCC"] = fairScoreErrorRates["MCC"]
     errDiffs["raw - fair FPR"] = rawScoreErrorRates["FPR"] - fairScoreErrorRates["FPR"]
     errDiffs["raw - fair FNR"] = rawScoreErrorRates["FNR"] - fairScoreErrorRates["FNR"]
+    errDiffs["raw - fair MCC"] = rawScoreErrorRates["MCC"] - fairScoreErrorRates["MCC"]
 
     resultString += "\n\n\n================================================\nERROR RATE DIFFERENCES \n================================================\n"
     resultString += errDiffs.to_string()
@@ -84,54 +99,72 @@ def evaluate(data: pd.DataFrame, pathToResult: str) -> None:
 
 
 def evaluateModelPerformanceAndErrorRates(data: pd.DataFrame, scoreAttr: str) -> Tuple[str, pd.DataFrame, pd.DataFrame]:
-    modelPerformances = pd.DataFrame(columns=["Accuracy", "Precision", "Recall"])
-    allErrorRates = pd.DataFrame(columns=["FPR", "FNR"])
+    model_performances = pd.DataFrame(columns=["Accuracy", "Precision", "Recall"])
+    all_error_rates = pd.DataFrame(columns=["FPR", "FNR", "MCC"])
 
     # collect model performances
     report = skmetr.classification_report(data["groundTruthLabel"], data[scoreAttr], output_dict=True)
-    modelPerformances.loc["all"] = {
+    model_performances.loc["all"] = {
         "Precision": round(report.get("weighted avg").get("precision"), DECIMAL_PLACES),
         "Recall": round(report.get("weighted avg").get("recall"), DECIMAL_PLACES),
         "Accuracy": round(report.get("accuracy"), DECIMAL_PLACES),
     }
 
     # calc error rates
-    confusionMatrixAll = pd.crosstab(data["groundTruthLabel"], data[scoreAttr], margins=True)
-    falsePositiveRate = round(confusionMatrixAll.loc[0, 1] / confusionMatrixAll.loc[0, "All"], DECIMAL_PLACES)
-    falseNegativeRate = round(confusionMatrixAll.loc[1, 0] / confusionMatrixAll.loc[1, "All"], DECIMAL_PLACES)
-    allErrorRates.loc["all"] = {"FPR": falsePositiveRate, "FNR": falseNegativeRate}
+    confusion_matrix_all = pd.crosstab(data["groundTruthLabel"], data[scoreAttr], margins=True)
+    tn_all = confusion_matrix_all.loc[0, 0]
+    fp_all = confusion_matrix_all.loc[0, 1]
+    fn_all = confusion_matrix_all.loc[1, 0]
+    tp_all = confusion_matrix_all.loc[1, 1]
+    matthews_correlation_coefficient = (tp_all * tn_all - fp_all * fn_all) / np.sqrt(
+        (tp_all + fp_all) * (tp_all + fn_all) * (tn_all + fp_all) * (tn_all + fn_all)
+    )
+    false_positive_rate = round(fp_all / (fp_all + tn_all), DECIMAL_PLACES)
+    false_negative_rate = round(fn_all / (fn_all + tp_all), DECIMAL_PLACES)
+    all_error_rates.loc["all"] = {
+        "FPR": false_positive_rate,
+        "FNR": false_negative_rate,
+        "MCC": matthews_correlation_coefficient,
+    }
 
     # print stuff
-    resultString = "\nERROR RATES ALL INDIVIDUALS \n====================================\n"
-    resultString += skmetr.classification_report(data["groundTruthLabel"], data[scoreAttr]) + "\n\n"
-    resultString += str(confusionMatrixAll) + "\n\n"
+    result_string = "\nERROR RATES ALL INDIVIDUALS \n====================================\n"
+    result_string += skmetr.classification_report(data["groundTruthLabel"], data[scoreAttr]) + "\n\n"
+    result_string += str(confusion_matrix_all) + "\n\n"
 
     for group in sorted(data["group"].unique()):
-        groupData = data[data["group"] == group]
+        group_data = data[data["group"] == group]
         # collect model performances
-        report = skmetr.classification_report(groupData["groundTruthLabel"], groupData[scoreAttr], output_dict=True)
-        modelPerformances.loc[group] = {
+        report = skmetr.classification_report(group_data["groundTruthLabel"], group_data[scoreAttr], output_dict=True)
+        model_performances.loc[group] = {
             "Precision": round(report.get("weighted avg").get("precision"), DECIMAL_PLACES),
             "Recall": round(report.get("weighted avg").get("recall"), DECIMAL_PLACES),
             "Accuracy": round(report.get("accuracy"), DECIMAL_PLACES),
         }
 
         # calc error rates
-        confusionMatrixGroup = pd.crosstab(groupData["groundTruthLabel"], groupData[scoreAttr], margins=True)
-        falsePositiveRateGroup = round(
-            confusionMatrixGroup.loc[0, 1] / confusionMatrixGroup.loc[0, "All"], DECIMAL_PLACES
+        confusion_matrix_group = pd.crosstab(group_data["groundTruthLabel"], group_data[scoreAttr], margins=True)
+        tn_group = confusion_matrix_group.loc[0, 0]
+        fp_group = confusion_matrix_group.loc[0, 1]
+        fn_group = confusion_matrix_group.loc[1, 0]
+        tp_group = confusion_matrix_group.loc[1, 1]
+        false_positive_rate_group = round(fp_group / (fp_group + tn_group), DECIMAL_PLACES)
+        false_negative_rate_group = round(fn_group / (fn_group + tp_group), DECIMAL_PLACES)
+        matthews_correlation_coefficient_group = (tp_group * tn_group - fp_group * fn_group) / np.sqrt(
+            (tp_group + fp_group) * (tp_group + fn_group) * (tn_group + fp_group) * (tn_group + fn_group)
         )
-        falseNegativeRateGroup = round(
-            confusionMatrixGroup.loc[1, 0] / confusionMatrixGroup.loc[1, "All"], DECIMAL_PLACES
-        )
-        allErrorRates.loc[group] = {"FPR": falsePositiveRateGroup, "FNR": falseNegativeRateGroup}
+        all_error_rates.loc[group] = {
+            "FPR": false_positive_rate_group,
+            "FNR": false_negative_rate_group,
+            "MCC": matthews_correlation_coefficient_group,
+        }
 
         # print
-        resultString += "ERROR RATES GROUP " + str(group) + " INDIVIDUALS \n====================================\n"
-        resultString += skmetr.classification_report(groupData["groundTruthLabel"], groupData[scoreAttr]) + "\n\n"
-        resultString += str(confusionMatrixGroup) + "\n\n"
+        result_string += "ERROR RATES GROUP " + str(group) + " INDIVIDUALS \n====================================\n"
+        result_string += skmetr.classification_report(group_data["groundTruthLabel"], group_data[scoreAttr]) + "\n\n"
+        result_string += str(confusion_matrix_group) + "\n\n"
 
-    return resultString, modelPerformances, allErrorRates
+    return result_string, model_performances, all_error_rates
 
 
 def evaluateWithGLM(scoreAttr: str, data: pd.DataFrame) -> str:
